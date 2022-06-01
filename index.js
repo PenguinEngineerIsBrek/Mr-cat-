90160e28c12a222bf72a8c590dc864825744c5ed
PenguinEngineerIsBrek <almightypengueng@gmail.com>
2022-06-01 21:05:58 +0200

    dodo caca
    lamboodada

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const aboutbutton = document.getElementById("abtmebtn")
 aboutbutton.onclick = function(){
     window.scrollTo(0, 900)
 }
+const dcbtn = document.getElementById("dcbtn")
+dcbtn.onclick = function(){
+    window.scrollTo(0, 1700)
+}
+const creditbtn = document.getElementById("creditbtn")
+creditbtn.onclick = function(){
+    window.scrollTo(0,3000)
+}
